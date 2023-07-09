756459149468a7602854900a2e5cdfaa2b03d9fd
ehabyahya <ehabyahyagoda@gmail.com>
2023-07-09 21:44:54 +0300

    adding synch ,asynch and chain functions in ADC

--- 3.MCAL/5_ADC/ADC_interface.h
+++ 3.MCAL/5_ADC/ADC_interface.h
@@ -1,14 +1,57 @@
-#ifndef ADC_INTERFACE_H
-#define ADC_INTERFACE_H
+#ifndef			ADC_INTERFACE_H
+#define			ADC_INTERFACE_H
 
+typedef enum
+{
+	ADC0_SINGLE_ENDED=0,
+	ADC1_SINGLE_ENDED,
+	ADC2_SINGLE_ENDED,
+	ADC3_SINGLE_ENDED,
+	ADC4_SINGLE_ENDED,
+	ADC5_SINGLE_ENDED,
+	ADC6_SINGLE_ENDED,
+	ADC7_SINGLE_ENDED,
+	ADC0_POS_ADC0_NEG_10X_GAIN,
+	ADC1_POS_ADC0_NEG_10X_GAIN,
+	ADC0_POS_ADC0_NEG_200X_GAIN,
+	ADC1_POS_ADC0_NEG_200X_GAIN,
+	ADC2_POS_ADC2_NEG_10X_GAIN,
+	ADC3_POS_ADC2_NEG_10X_GAIN,
+	ADC2_POS_ADC2_NEG_200X_GAIN,
+	ADC3_POS_ADC2_NEG_200X_GAIN,
+	ADC0_POS_ADC1_NEG_1X_GAIN,
+	ADC1_POS_ADC1_NEG_1X_GAIN,
+	ADC2_POS_ADC1_NEG_1X_GAIN,
+	ADC3_POS_ADC1_NEG_1X_GAIN,
+	ADC4_POS_ADC1_NEG_1X_GAIN,
+	ADC5_POS_ADC1_NEG_1X_GAIN,
+	ADC6_POS_ADC1_NEG_1X_GAIN,
+	ADC7_POS_ADC1_NEG_1X_GAIN,
+	ADC0_POS_ADC2_NEG_1X_GAIN,
+	ADC1_POS_ADC2_NEG_1X_GAIN,
+	ADC2_POS_ADC2_NEG_1X_GAIN,
+	ADC3_POS_ADC2_NEG_1X_GAIN,
+	ADC4_POS_ADC2_NEG_1X_GAIN,
+	ADC5_POS_ADC2_NEG_1X_GAIN,
+	V1_22__SINGLE_ENDED,
+	V0__SINGLE_ENDED
+}ADC_Channel_t;
 
+typedef struct
+{
+	uint8   ADC_u8NumConversions;
+	ADC_Channel_t*  ADC_peChannels;
+	uint16*  ADC_pu16Results;
+	void (*ADC_pfvNotificationFunction)(void);
+}ADC_Chain_t;
 
-void ADC_voidInit(void);
-uint8 ADC_u8GetChannelReading(uint8 copy_u8Channel);
-
-
-
-
+void   ADC_voidInit(void);
+void   ADC_voidEnable(void);
+void   ADC_voidDisable(void);
+uint8 ADC_u16StartConversionSynch(ADC_Channel_t  copy_eChannel,uint16* copy_pu16DigitalResult);
+uint8 ADC_u16StartConversionASynch(ADC_Channel_t  copy_eChannel,uint16* copy_pu16DigitalResult, void (*copy_pfvNotificationFunc)(void));
+uint8 ADC_u16StartChainConversionASynch(const ADC_Chain_t* copy_psChainObject);
+uint16 ADC_u16GetAnalogVolt(uint16 copy_u16ADC_Reading);
 
 
 
--- 3.MCAL/5_ADC/ADC_reg.h
+++ 3.MCAL/5_ADC/ADC_reg.h
@@ -1,22 +1,24 @@
-#ifndef ADC_REGISTER_H_
-#define ADC_REGISTER_H_
-
-#define ADMUX                *((volatile uint8*)0x27)	 /*ADC multilpexer selection register*/
-#define ADMUX_REFS1			 7                           /*Reference selection Bit1*/
-#define ADMUX_REFS0			 6                           /*Reference selection Bit0*/
-#define ADMUX_ADLAR			 5                           /*Left adjust result*/
-
-#define ADCSRA               *((volatile uint8*)0x26)	/*ADC control and status registerA*/
-#define ADCSRA_ADEN			 7                          /*Enable*/
-#define ADCSRA_ADSC			 6                          /*Start conversion*/
-#define ADCSRA_ADATE		 5                          /*Auto trigger enable*/
-#define ADCSRA_ADIF			 4                          /*Interrupt flag*/
-#define ADCSRA_ADIE			 3                          /*Interrupt enable*/
+#ifndef 	ADC_REG_H
+#define 	ADC_REG_H
 
 
-#define ADCH                 *((volatile uint8*)0x25)		/*Data high register*/
-#define ADCL                 *((volatile uint8*)0x24)		/*Data low register*/
-#define ADC					 *((volatile uint16*)0x24)		/*ADC data register*/
+#define  	ADMUX			*((volatile uint8*)0x27)		/*ADC multiplexer selection register*/
+#define     ADMUX_REFS1		7u								/*Reference selection Bit1*/
+#define     ADMUX_REFS0		6u								/*Reference selection Bit0*/
+#define	    ADMUX_ADLAR		5u								/*Left adjust result*/
+
+
+#define  	ADCSRA			*((volatile uint8*)0x26)		/*ADC control and status registerA*/
+#define     ADCSRA_ADEN		7u								/*Enable*/
+#define     ADCSRA_ADSC		6u								/*Start conversion*/
+#define     ADCSRA_ADATE	5u								/*Auto trigger enable*/
+#define     ADCSRA_ADIF		4u								/*Interrupt flag*/
+#define     ADCSRA_ADIE		3u								/*Interrupt enable*/
+
+
+#define     ADCH			*((volatile uint8*)0x25)		/*Data high register*/
+#define     ADCL			*((volatile uint8*)0x24)		/*Data low register*/
+#define     ADC				*((volatile uint16*)0x24)		/*ADC data register*/
 
 
 #endif
